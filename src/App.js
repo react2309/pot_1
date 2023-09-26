34afac592f4b8721e5010035d16f2020b32cfcaa
YKyungmin <xcv556865@gmail.com>
2023-09-26 14:43:43 +0900

    route로 특정 url에 특정 컴포넌트 맵핑

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './styles/Global.scss';
+import { Route } from 'react-router-dom';
 import Header from './components/common/header/Header';
 import Department from './components/sub/department/Department';
+import Youtube from './components/sub/youtube/Youtube';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<Department />
+			<Route path='/department'>
+				<Department />
+			</Route>
+			<Route path='/youtube'>
+				<Youtube />
+			</Route>
 		</>
 	);
 }
